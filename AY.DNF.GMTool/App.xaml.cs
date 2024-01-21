44359ce3e4bffdeb2b2a49f728829511ac65b05d
AsakuraYou <15510802632@163.com>
2024-01-21 17:58:45 +0800

    添加项目文件。

---
+++ AY.DNF.GMTool/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using AY.DNF.GMTool.Views;
+using Prism.Ioc;
+using System.Windows;
+
+namespace AY.DNF.GMTool
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}

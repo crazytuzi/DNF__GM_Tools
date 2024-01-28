ecbd75bc880bedfacd2d196c81f55f4164cfdf82
AsakuraYou <15510802632@163.com>
2024-01-28 16:49:11 +0800

    预留活动管理模块

---
+++ AY.DNF.GMTool.ActivityEvent/ActivityEventModule.cs
@@ -0,0 +1,18 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+
+namespace AY.DNF.GMTool.ActivityEvent
+{
+    class ActivityEventModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<ActivityEventModule>(nameof(ActivityEventModule));
+        }
+    }
+}

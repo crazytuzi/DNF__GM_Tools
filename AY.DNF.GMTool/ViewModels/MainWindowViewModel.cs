3f2108911aaffdbf9511a3025ba9789be3ee7090
AsakuraYou <15510802632@163.com>
2024-02-21 09:08:46 +0800

    版本号

--- AY.DNF.GMTool/ViewModels/MainWindowViewModel.cs
+++ AY.DNF.GMTool/ViewModels/MainWindowViewModel.cs
@@ -49,7 +49,7 @@ namespace AY.DNF.GMTool.ViewModels
             set { SetProperty(ref _title, value); }
         }
 
-        private string _version = "1.0.2024xxxx";
+        private string _version = "1.0.20240221";
         /// <summary>
         /// 显示的自定义版本号
         /// </summary>
